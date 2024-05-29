97f1372122b8b61b37f70193eb28a3ab79eb0e9a
Dan <dci1996@msn.com>
2024-05-29 12:01:07 -0400

    Upload correct files

--- 1. Practice Problems/Week 1/debug/debug.c
+++ 1. Practice Problems/Week 1/debug/debug.c
@@ -1,14 +1,15 @@
 // Become familiar wih C syntax
 // Learn to debug buggy code
 
-//#include <cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     // Ask for your name and where live
-    name = get_string("What is your name? ")
-    location = get_string("Where do you live? ")
+    string name = get_string("What is your name? ");
+    string location = get_string("Where do you live? ");
 
     // Say hello
-    print("Hello, %i, from %i!", name, location)
+    printf("Hello, %s, from %s!", name, location);
 }
